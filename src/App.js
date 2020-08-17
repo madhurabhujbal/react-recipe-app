43dc27cd57f028e2f787ec7963d5dbddad60e3ca
Madhura <madhura.bhujbal@gmail.com>
2020-08-17 10:29:25 +0100

    Delete default return function and add it in new arrow function App

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello React</h1>
-    </div>
-  );
+  const App = () => {
+    // const APP_ID = "449006bf";
+    // const APP_KEY = "ab47742811a6686862dfffde29f0722f";
+    return (
+      <div className="App">
+        <h1>Hello React</h1>
+      </div>
+    );
+  };
 }
 
 export default App;
