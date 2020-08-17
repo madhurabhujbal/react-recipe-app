a68b0a9c4700b79b033578dacf620f0f6bb827a1
Madhura <madhura.bhujbal@gmail.com>
2020-08-17 11:59:06 +0100

    Import useEffect and useState from React and write basic functions to display them

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 const App = () => {
@@ -8,12 +7,21 @@ const App = () => {
   const APP_KEY = "ab47742811a6686862dfffde29f0722f";
   const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log("Effect has been run");
+  });
+
   return (
     <div className="App">
       <form className="search-form">
         <input className="search-bar" type="text" />
-        <button className="search-button">Search</button>
+        <button className="search-button" type="submit">
+          Search
+        </button>
       </form>
+      {/* <h1 onClick={() => setCounter(counter + 1)}> {counter} </h1> */}
     </div>
   );
 };
