f188062851e25bbdd0ac270914bd4250ba5efa0b
Madhura <madhura.bhujbal@gmail.com>
2020-08-17 14:52:43 +0100

    Get all recipes in const recipes using setRecipes()

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 const App = () => {
   //id and key for edamam.com REST API for recipe request
   const APP_ID = "449006bf";
   const APP_KEY = "ab47742811a6686862dfffde29f0722f";
+
+  const [recipes, setRecipes] = useState([]);
 
   useEffect(() => {
     //runs every time page renders
@@ -20,7 +22,7 @@ const App = () => {
       `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
     );
     const data = await response.json();
-    console.log(data.hits);
+    setRecipes(data.hits);
   };
 
   return (
