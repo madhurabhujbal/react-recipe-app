be62a1fe782e7c37295b4b5dbe8e676a7b7136f2
Madhura <madhura.bhujbal@gmail.com>
2020-08-17 10:44:23 +0100

    Delete previous App function

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  const App = () => {
-    //id and key for edamam.com REST API for recipe request
-    const APP_ID = "449006bf";
-    const APP_KEY = "ab47742811a6686862dfffde29f0722f";
-    const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
-    return (
-      <div className="App">
-        <h1>Hello React</h1>
-      </div>
-    );
-  };
-}
+const App = () => {
+  //id and key for edamam.com REST API for recipe request
+  const APP_ID = "449006bf";
+  const APP_KEY = "ab47742811a6686862dfffde29f0722f";
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  return (
+    <div className="App">
+      <h1>Hello React</h1>
+    </div>
+  );
+};
 
 export default App;
