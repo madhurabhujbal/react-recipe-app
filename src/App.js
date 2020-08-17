691e997d7c26a2ab9d6fedee939a134aae8e3978
Madhura <madhura.bhujbal@gmail.com>
2020-08-17 10:41:16 +0100

    Add the search query with API id and key

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     //id and key for edamam.com REST API for recipe request
     const APP_ID = "449006bf";
     const APP_KEY = "ab47742811a6686862dfffde29f0722f";
+    const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
     return (
       <div className="App">
         <h1>Hello React</h1>
