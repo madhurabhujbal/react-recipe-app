d9bb0d49d5e6b83178eb525b90182524b7fa911f
Madhura <madhura.bhujbal@gmail.com>
2020-08-17 10:33:34 +0100

    Add API id and key for recipe app

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import "./App.css";
 
 function App() {
   const App = () => {
-    // const APP_ID = "449006bf";
-    // const APP_KEY = "ab47742811a6686862dfffde29f0722f";
+    //id and key for edamam.com REST API for recipe request
+    const APP_ID = "449006bf";
+    const APP_KEY = "ab47742811a6686862dfffde29f0722f";
     return (
       <div className="App">
         <h1>Hello React</h1>
