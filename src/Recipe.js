8bcf08ecb3c8d5e693b5b853915e45178adb6dd0
Madhura <madhura.bhujbal@gmail.com>
2020-08-18 10:30:46 +0100

    Add customized styling to images

--- src/Recipe.js
+++ src/Recipe.js
@@ -11,7 +11,7 @@ const Recipe = ({ title, calories, image, ingredients }) => {
         ))}
       </ol>
       <p>{"Calories: " + calories}</p>
-      <img src={image} alt="recipe-image" />
+      <img className={style.image} src={image} alt="recipe-image" />
     </div>
   );
 };
