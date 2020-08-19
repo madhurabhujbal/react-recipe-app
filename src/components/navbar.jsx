66e9e0b8a14d631075cf9e00884490deee29c052
Madhura <madhura.bhujbal@gmail.com>
2020-08-19 15:11:15 +0100

    Add search-bar and search-button elements to the navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,21 @@
 import React from "react";
 
 const Navbar = () => {
-  return "Got into navbar";
+  return (
+    <nav
+      className="navbar navbar-dark bg-dark"
+      style={{ minHeight: "180px", width: "100%", paddingTop: "20px" }}
+    >
+      <input
+        className="search-bar"
+        type="text"
+        // value={search}
+        // onChange={updateSearch}
+      />
+      <button className="search-button" type="submit">
+        Search
+      </button>
+    </nav>
+  );
 };
 export default Navbar;
