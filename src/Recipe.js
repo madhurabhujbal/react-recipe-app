90a4df3bac5f95b85c16fd2f7cfe5a434c830f91
Madhura <madhura.bhujbal@gmail.com>
2020-08-19 09:50:37 +0100

    Add code to display msg if dietlabel is not available

--- src/Recipe.js
+++ src/Recipe.js
@@ -2,6 +2,7 @@ import React from "react";
 import style from "./recipe.module.css";
 
 const Recipe = ({ title, calories, dietLabels, image, ingredients }) => {
+  let dl = dietLabels.length !== 0 ? dietLabels : "Currently not available";
   return (
     <div className={style.recipe}>
       <h1>{title}</h1>
@@ -12,7 +13,7 @@ const Recipe = ({ title, calories, dietLabels, image, ingredients }) => {
       </ol>
       <p style={{ color: "red", fontWeight: "bold" }}>
         {"Calories: " + calories.toFixed(2) + "C"} &nbsp;
-        {"Diet Labels: " + dietLabels}
+        {"Diet Labels: " + dl}
       </p>
       <img className={style.image} src={image} alt="recipe-image" />
     </div>
