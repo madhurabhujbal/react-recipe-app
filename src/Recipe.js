c27f69751bc75065bbb4d4ddabaf77ae78dc874b
Madhura <madhura.bhujbal@gmail.com>
2020-08-19 10:01:42 +0100

    Simplify the code to write conditional operator in one line

--- src/Recipe.js
+++ src/Recipe.js
@@ -2,7 +2,6 @@ import React from "react";
 import style from "./recipe.module.css";
 
 const Recipe = ({ title, calories, dietLabels, image, ingredients }) => {
-  let dl = dietLabels.length !== 0 ? dietLabels : "Currently not available";
   return (
     <div className={style.recipe}>
       <h1>{title}</h1>
@@ -13,7 +12,8 @@ const Recipe = ({ title, calories, dietLabels, image, ingredients }) => {
       </ol>
       <p style={{ color: "red", fontWeight: "bold" }}>
         {"Calories: " + calories.toFixed(2) + "C"} &nbsp;
-        {"Diet Labels: " + dl}
+        {"Diet Labels: " +
+          (dietLabels.length !== 0 ? dietLabels : "Currently not available")}
       </p>
       <img className={style.image} src={image} alt="recipe-image" />
     </div>
