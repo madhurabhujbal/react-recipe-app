61134344de8ef219690e58ffedd41cca3618d824
Madhura <madhura.bhujbal@gmail.com>
2020-08-20 09:22:28 +0100

    Add destructuring way of extracting methods from props

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,10 +2,10 @@ import React, { Component } from "react";
 
 export default class Navbar extends Component {
   render() {
-    const getSearch = this.props.getSearch;
-    const updateSearch = this.props.updateSearch;
-    const search = this.props.search;
-    // const { getSearch, updateSearch, search } = this.props;
+    // const getSearch = this.props.getSearch;
+    // const updateSearch = this.props.updateSearch;
+    // const search = this.props.search;
+    const { getSearch, updateSearch, search } = this.props;
     return (
       <div>
         <form onSubmit={getSearch} className="search-form">
