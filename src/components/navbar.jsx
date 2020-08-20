ef4d39060f6fbfba929fa7be6e350daeacc22440
Madhura <madhura.bhujbal@gmail.com>
2020-08-20 09:06:28 +0100

    Add code for rendering navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Navbar = () => {
-  return (
-    <nav
-      className="navbar navbar-dark bg-dark"
-      style={{ minHeight: "180px", width: "100%", paddingTop: "20px" }}
-    >
-      <input className="search-bar" type="text" />
-      <button className="search-button" type="submit">
-        Search
-      </button>
-    </nav>
-  );
-};
-export default Navbar;
+export default class Navbar extends Component {
+  render() {
+    const { getSearch, updateSearch, search } = this.props;
+    return (
+      <div>
+        <form onSubmit={getSearch} className="search-form">
+          <input
+            className="search-bar"
+            type="text"
+            value={search}
+            onChange={updateSearch}
+          />
+          <button className="search-button" type="submit">
+            Search
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+// const Navbar = () => {
+//   const sayHello = this.props.sayHello;
+//   sayHello();
+//   return (
+//     <nav
+//       className="navbar navbar-dark bg-dark"
+//       style={{ minHeight: "180px", width: "100%", paddingTop: "20px" }}
+//     >
+//       <input className="search-bar" type="text" />
+//       <button className="search-button" type="submit">
+//         Search
+//       </button>
+//     </nav>
+//   );
+// };
